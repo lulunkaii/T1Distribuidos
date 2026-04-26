3792a143beb097c86e78fb8e426ce73cbb7de48e
amonlop <montlopanto@gmail.com>
2026-04-26 18:01:04 -0400

    feat: parallel_brms listo. Se modificó el makefile para ejectutar perf en los experimentos

--- algorithms/parallel_brms.cpp
+++ algorithms/parallel_brms.cpp
@@ -1,14 +1,153 @@
-/** *
+/**
  * @file parallel_brms.cpp
  * @author amonlop
- * @brief Binary and Rank Mergesort parallel implementation
+ * @brief Implementación paralela de Binary and Rank Mergesort (BRMS).
+ * El algoritmo descompone el merge de dos arreglos ordenados seleccionando
+ * pivotes del segundo arreglo y encontrando su posición (rank) en el primero
+ * mediante búsqueda binaria, permitiendo realizar merges independientes en paralelo.
 **/
 
 #include <iostream>
 #include <vector>
+#include <cmath>
+#include <algorithm>
+#include <omp.h>
 
+#include "seq_mergesort.h"
 #include "parallel_brms.h"
 
-void parallelBRMS(std::vector<int>& array, int left, int right){
-    //función principal
+static const int CUT = 4096;     //umbral para utilizar sequential merge
+static const int C_FACTOR = 4;          // factor c * logn
+
+
+// Calcula los rangos (ranks) de los pivotes de B dentro de A.
+void partition(const std::vector<int>& A, int aLow, int aHigh,
+            const std::vector<int>& B, int bLow, int bHigh,
+            std::vector<int>& ranks,
+            int logN) {
+
+    int sizeA = aHigh - aLow;
+    int numPartitions = ranks.size() - 1;
+
+    ranks[0] = 0;
+    ranks[numPartitions] = sizeA;
+
+    //la operación se puede hacer tanto paralela como secuencial, y depende del N
+    #pragma omp parallel for schedule(static)
+    for (int i = 1; i < numPartitions; i++) {
+        int pivot = B[bLow + i * logN];
+
+        auto it = std::lower_bound(A.begin() + aLow,
+                                A.begin() + aHigh,
+                                pivot);
+
+        ranks[i] = std::distance(A.begin() + aLow, it);
+    }
+}
+
+
+//versión alternativa de merge (para evitar uso de memoria innecesaria del merge definido en seq_mergesort.cpp, y con esto reducir los cache misses)
+void seqMerge(const std::vector<int>& A, int aLow, int aHigh,
+            const std::vector<int>& B, int bLow, int bHigh,
+            std::vector<int>& output, int outLow) {
+
+    int i = aLow, j = bLow, k = outLow;
+
+    while (i < aHigh && j < bHigh)
+        output[k++] = (A[i] <= B[j]) ? A[i++] : B[j++];
+
+    while (i < aHigh) output[k++] = A[i++];
+    while (j < bHigh) output[k++] = B[j++];
+}
+
+// merge paralelo considerando particiones y ranks binarios
+void parMergeBRMS(const std::vector<int>& A, int aLow, int aHigh,
+                    const std::vector<int>& B, int bLow, int bHigh,
+                    std::vector<int>& output, int outLow) {
+
+    int sizeA = aHigh - aLow;
+    int sizeB = bHigh - bLow;
+    int totalN = sizeA + sizeB;
+
+    if (sizeA == 0) {
+        std::copy(B.begin() + bLow, B.begin() + bHigh, output.begin() + outLow);
+        return;
+    }
+    if (sizeB == 0) {
+        std::copy(A.begin() + aLow, A.begin() + aHigh, output.begin() + outLow);
+        return;
+    }
+
+    if (totalN <= CUT) {
+        seqMerge(A, aLow, aHigh, B, bLow, bHigh, output, outLow);
+        return;
+    }
+
+    int logN = std::max(16, (int)std::log2(totalN));
+    int numTasks = (sizeB + logN - 1) / logN;
+
+    std::vector<int> ranks(numTasks + 1);
+
+    partition(A, aLow, aHigh, B, bLow, bHigh, ranks, logN);
+
+    for (int i = 0; i < numTasks; i++) {
+
+        int aStart = aLow + ranks[i];
+        int aEnd   = aLow + ranks[i + 1];
+
+        int bStart = bLow + i * logN;
+        int bEnd   = std::min(bLow + (i + 1) * logN, bHigh);
+
+        int outStart = outLow + ranks[i] + (i * logN);
+
+        #pragma omp task shared(A, B, output, ranks)
+        {
+            int subSizeA = aEnd - aStart;
+
+            if (subSizeA <= C_FACTOR * logN) {
+                seqMerge(A, aStart, aEnd,
+                        B, bStart, bEnd,
+                        output, outStart);
+            } else {
+                // se intercambian A y B
+                parMergeBRMS(B, bStart, bEnd,
+                            A, aStart, aEnd,
+                            output, outStart);
+            }
+        }
+    }
+
+    #pragma omp taskwait
+}
+
+
+void parallelBRMS(std::vector<int>& array, int left, int right) {
+
+    if (left >= right) return;
+
+    //se utiliza mergesort secuencial si el tamaño es menor al umbral CUT
+    if (right - left + 1 <= CUT) {
+        sequentialMergeSort(array, left, right);
+        return;
+    }
+
+    int mid = left + (right - left) / 2;
+
+    #pragma omp task default(none) shared(array) firstprivate(left, mid)
+    parallelBRMS(array, left, mid);
+
+    #pragma omp task default(none) shared(array) firstprivate(mid, right)
+    parallelBRMS(array, mid + 1, right);
+
+    #pragma omp taskwait
+
+    //espacio temporal para almacenar la mezcla de las dos mitades
+    std::vector<int> output(right - left + 1);
+
+    parMergeBRMS(array, left, mid + 1,
+                array, mid + 1, right + 1,
+                output, 0);
+
+    //cpia de los datos ordenados de vuelta al arreglo original
+    std::copy(output.begin(), output.end(), array.begin() + left);
 }
--- makefile
+++ makefile
@@ -1,14 +1,22 @@
+##agregar esta linea si tira error en la ejecución de perf
+##sudo sysctl -w kernel.perf_event_paranoid=-1
+##sudo sysctl -w kernel.kptr_restrict=0
+
+
+##Si no quieres que se ejecute dos veces cada vez que hagas un test en un algoritmo paralelo, 
+## puedes comentar la línea de perf c2c en el Makefile y dejar solo el perf stat (o viceversa).
 CXX      = g++
 CXXFLAGS = -O2 -fopenmp -std=c++17 -I algorithms/
 
 TARGET = mergesort
 SRCS   = main.cpp \
-	algorithms/seq_mergesort.cpp \
-	algorithms/parallel_mergesort.cpp \
-	algorithms/seq_kway.cpp \
-	algorithms/parallel_kway.cpp \
-	algorithms/parallel_brms.cpp
+    algorithms/seq_mergesort.cpp \
+    algorithms/parallel_mergesort.cpp \
+    algorithms/seq_kway.cpp \
+    algorithms/parallel_kway.cpp \
+    algorithms/parallel_brms.cpp
 
+# --- Argumentos por defecto ---
 RUNS    ?= 5
 EXP_LO  ?= 20
 EXP_HI  ?= 26
@@ -16,41 +24,63 @@ EXP_ST  ?= 2
 P_LO    ?= 0
 P_HI    ?= 3
 
+# Directorios
+OUT_DIR = output
+PERF_DIR = perf_outputs
+
+# Eventos de monitoreo
+EVENTS = cycles,instructions,cache-references,cache-misses,stalled-cycles-backend
+
+# Comandos de Perf
+P_STAT = perf stat -e $(EVENTS) -o $(PERF_DIR)/stat_$@.txt --append
+P_C2C  = perf c2c record -o $(PERF_DIR)/c2c_$@.data
+P_C2C_REP = perf c2c report -i $(PERF_DIR)/c2c_$@.data --stdio > $(PERF_DIR)/c2c_$@.txt
+# Comando para borrar el binario .data
+P_RM_DATA = rm -f $(PERF_DIR)/c2c_$@.data
+
 $(TARGET): $(SRCS)
 	$(CXX) $(CXXFLAGS) -o $(TARGET) $(SRCS)
 
-run: $(TARGET)
-	mkdir -p output
-	./$(TARGET) output/results.csv par_mergesort $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) $(P_LO) $(P_HI)
+init:
+	mkdir -p $(OUT_DIR)
+	mkdir -p $(PERF_DIR)
 
-seq_ms: $(TARGET)
-	mkdir -p output
-	./$(TARGET) output/seq_mergesort.csv seq_mergesort $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) 0 0
+# --- Algoritmos Secuenciales ---
+seq_ms: $(TARGET) init
+	$(P_STAT) ./$(TARGET) $(OUT_DIR)/seq_mergesort.csv seq_mergesort $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) 0 0
 
-par_ms: $(TARGET)
-	mkdir -p output
-	./$(TARGET) output/par_mergesort.csv par_mergesort $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) $(P_LO) $(P_HI)
+seq_kw: $(TARGET) init
+	$(P_STAT) ./$(TARGET) $(OUT_DIR)/seq_kway.csv seq_kway $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) 0 0
 
-seq_kw: $(TARGET)
-	mkdir -p output
-	./$(TARGET) output/seq_kway.csv seq_kway $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) 0 0
+# --- Algoritmos Paralelos ---
+par_ms: $(TARGET) init
+	$(P_STAT) ./$(TARGET) $(OUT_DIR)/par_mergesort.csv par_mergesort $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) $(P_LO) $(P_HI)
+	$(P_C2C) ./$(TARGET) $(OUT_DIR)/par_mergesort.csv par_mergesort $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) $(P_LO) $(P_HI)
+	$(P_C2C_REP)
+	$(P_RM_DATA)
 
-par_kw: $(TARGET)
-	mkdir -p output
-	./$(TARGET) output/par_kway.csv par_kway $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) $(P_LO) $(P_HI)
+par_kw: $(TARGET) init
+	$(P_STAT) ./$(TARGET) $(OUT_DIR)/par_kway.csv par_kway $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) $(P_LO) $(P_HI)
+	$(P_C2C) ./$(TARGET) $(OUT_DIR)/par_kway.csv par_kway $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) $(P_LO) $(P_HI)
+	$(P_C2C_REP)
+	$(P_RM_DATA)
 
-par_br: $(TARGET)
-	mkdir -p output
-	./$(TARGET) output/par_brms.csv par_brms $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) $(P_LO) $(P_HI)
+par_br: $(TARGET) init
+	$(P_STAT) ./$(TARGET) $(OUT_DIR)/par_brms.csv par_brms $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) $(P_LO) $(P_HI)
+	$(P_C2C) ./$(TARGET) $(OUT_DIR)/par_brms.csv par_brms $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) $(P_LO) $(P_HI)
+	$(P_C2C_REP)
+	$(P_RM_DATA)
 
-all_experiments: $(TARGET)
-	mkdir -p output
-	./$(TARGET) output/seq_mergesort.csv seq_mergesort $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) 0 0
-	./$(TARGET) output/par_mergesort.csv par_mergesort $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) $(P_LO) $(P_HI)
-	./$(TARGET) output/seq_kway.csv      seq_kway      $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) 0 0
-	./$(TARGET) output/par_kway.csv      par_kway      $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) $(P_LO) $(P_HI)
-	./$(TARGET) output/par_brms.csv      par_brms      $(RUNS) $(EXP_LO) $(EXP_HI) $(EXP_ST) $(P_LO) $(P_HI)
+all_experiments: $(TARGET) init
+	@echo "Ejecutando experimentos..."
+	$(MAKE) seq_ms
+	$(MAKE) par_ms
+	$(MAKE) seq_kw
+	$(MAKE) par_kw
+	$(MAKE) par_br
 	python3 merge_results.py
 
 clean:
-	rm -f $(TARGET) output/*.csv
+	rm -f $(TARGET)
+	rm -rf $(OUT_DIR)
+	rm -rf $(PERF_DIR)
